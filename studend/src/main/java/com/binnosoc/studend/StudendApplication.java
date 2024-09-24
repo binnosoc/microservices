c6e2a7644335f4fc7e7f0f079970441790cac230
Boro Ousseni <bin.innosoc@gmail.com>
2024-09-24 16:47:04 +0100

    first commit

---
+++ studend/src/main/java/com/binnosoc/studend/StudendApplication.java
@@ -0,0 +1,13 @@
+package com.binnosoc.studend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudendApplication.class, args);
+	}
+
+}
---
+++ studend/src/test/java/com/binnosoc/studend/StudendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binnosoc.studend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
